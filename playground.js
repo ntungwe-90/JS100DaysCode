7ee848a0dbef5c74082718c59de074d7b6f65aaf
ntungwe-90 <ntongweirene@gmail.com>
2022-11-16 16:33:20 +0000

    day two data types and conditionals

--- playground.js
+++ playground.js
@@ -14,3 +14,34 @@ tipAmount = food * tipPercentage
 console.log(tipAmount)
 
 // DAY TWO
+// data types
+let foo = 45; //foo is a number
+foo ="bar"; //foo is now string
+foo = true;
+
+let x = 5;
+let y = 5;
+let z = 6;
+x = y
+x = z
+ //foo is now a boolean which can be either true or false
+let b = {firstName:"John", lastName:"Doe"}; //object
+
+const foot = 42;
+const result = foot + "1" // js coerces foot to a string, so it can be concatenated with the other operang
+console.log(result)
+
+//conditionals
+const age = 15;
+const state = "idaho"
+if(age >=16){
+console.log("you are old enough to take a test!")
+// the else if condition brings another contition different from the previousely set condition for example
+}else if(age >= 15 && state === "idaho" || state === "mountana"){
+console.log("you can take the test in your home state")
+}else{
+    console.log("you are too young to take the test")
+}
+
+// functions
+

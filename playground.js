01c29bf43421ea05d724877c621d46f73f39c6b9
ntungwe-90 <ntongweirene@gmail.com>
2022-11-16 08:50:35 +0000

    day one hundred days of code with Js

---
+++ playground.js
@@ -0,0 +1,16 @@
+// HUNDRED DAYS OF CODE CHALLENGE
+// DAY ONE
+console.log("hello world")
+console.log("irene ntongwe")
+
+// storing variables
+name ="suzy smith"
+console.log(name)
+
+// operators
+food = prompt("how much was the food")
+tipPercentage = 0.2
+tipAmount = food * tipPercentage
+console.log(tipAmount)
+
+// DAY TWO

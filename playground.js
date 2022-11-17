b86df8c5bf853a5d88e97ad8529e4eddfd01f06d
ntungwe-90 <ntongweirene@gmail.com>
2022-11-17 16:36:14 +0000

    day3 functions and objects

--- playground.js
+++ playground.js
@@ -31,6 +31,11 @@ const foot = 42;
 const result = foot + "1" // js coerces foot to a string, so it can be concatenated with the other operang
 console.log(result)
 
+//binding
+var name = "ayda";
+const greeting = "hello"
+console.log(greeting + '' + name)
+
 //conditionals
 const age = 15;
 const state = "idaho"
@@ -43,5 +48,29 @@ console.log("you can take the test in your home state")
     console.log("you are too young to take the test")
 }
 
-// functions
 
+// DAY THREE
+// functions
+console.log(Math.max(2, 4))
+console.log(Math.min(8, 9))
+
+
+function numbers(num1, num2){
+    return num1 * num2
+}
+
+//arrow function syntag
+let myfunction = () =>{
+
+}
+
+function alfa(c, d){
+    return c + d
+}
+alfa(10, 2)
+
+//OBJECTS
+const person ={
+    name:"mensah peace", country:"Burundi", eyes:"blue", age:39, height:1.55
+}
+console.log(person.name + person.age)
